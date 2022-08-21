cdca8baf2fa1e174e656dd646843f4e1ee7e2019
hahahalol9 <cmak7543@mail.ru>
2022-08-21 19:21:32 +0300

    chore: add web security configuration

---
+++ src/main/java/com/example/chatapp/services/UserDetailsServiceImplementation.java
@@ -0,0 +1,23 @@
+package com.example.chatapp.services;
+
+import com.example.chatapp.models.User;
+import com.example.chatapp.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImplementation implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        return user.orElseThrow(()->new UsernameNotFoundException("user with such name is not found"));
+    }
+}
