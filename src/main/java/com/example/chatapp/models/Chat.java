d28ad6b7b5073a792d01183b0080fd53ab150e16
hahahalol9 <cmak7543@mail.ru>
2022-08-31 20:12:12 +0300

    chore: update chat creator fetch type to lazy initialization

--- src/main/java/com/example/chatapp/models/Chat.java
+++ src/main/java/com/example/chatapp/models/Chat.java
@@ -2,10 +2,10 @@ package com.example.chatapp.models;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table(name = "chats")
@@ -15,11 +15,14 @@ public class Chat {
     @GeneratedValue
     @Id
     private Long id;
+    @CreationTimestamp
+    private Date creationDate;
     private String name;
-    @ManyToMany
-    private Set<User> participants;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable
+    private Set<User> participants = new HashSet<>();
     @ManyToOne
     private User creator;
-    @ElementCollection
+    @OneToMany(mappedBy = "chat",cascade = CascadeType.REMOVE)
     private List<ChatMessage> messages;
 }
--- src/main/java/com/example/chatapp/models/ChatUserEventMessage.java
+++
@@ -1,7 +0,0 @@
-package com.example.chatapp.models;
-
-public record ChatUserEventMessage(String username, ChatUserEventType userEvent){
-    public String createMessage(){
-        return userEvent.formatMessage(username);
-    }
-}
